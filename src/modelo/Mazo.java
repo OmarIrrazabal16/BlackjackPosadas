24be951837cdf1f5a90fc0a21faa24d65c019420
Desarrollo <Desarrollo@DESARROLLO07.local>
2024-05-27 09:22:30 +0200

    Commit inicial
    SEha añadido las calases Carta, Mazo y NoHayMasCartasException. Mano
    esta creada pero de momento vacía, hay que completarla

---
+++ src/modelo/Mazo.java
@@ -0,0 +1,48 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import modelo.Carta.Palo;
+
+public class Mazo {
+	
+	//Variables miembro
+	protected ArrayList<Carta> cartas;
+	
+	public Mazo() {
+	  //Creamos la lisat de cartas vacías
+		
+		this.cartas= new ArrayList<Carta>();
+	// Recorre la lista de palos
+		for (Palo p: Palo.values()) {
+			//Recorre todos los valores de la carta
+			for (int i=1; i<=13; i++) {
+				Carta carta = new Carta(i, p);
+				this.cartas.add(carta);
+			}
+			
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Mazo [cartas=" + cartas + "]";
+	}
+	
+	public void barajar() {
+		Collections.shuffle(cartas);
+	}
+	
+	public Carta solicitarCarta() throws NoHayMasCartasException {
+		if(this.cartas.size()==0) {
+			throw new NoHayMasCartasException();
+		}
+		//recogemos la primera carta del mazo de cartas
+		Carta c =this.cartas.get(0);
+		//eliminamos la carta recogida del mazo
+		this.cartas.remove(0);
+		return c;
+	}
+
+}
