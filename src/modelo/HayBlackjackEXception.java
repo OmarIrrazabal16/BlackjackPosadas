2b6c5503ffc28ef763c26a8a0ce472c20d4db029
Desarrollo <Desarrollo@DESARROLLO07.local>
2024-05-28 10:43:34 +0200

    Clases principales completas, se ha terminado de implementar la clase Mano.

---
+++ src/modelo/HayBlackjackEXception.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public class HayBlackjackEXception extends Exception {
+
+}
--- src/modelo/Mano.java
+++ src/modelo/Mano.java
@@ -11,10 +11,47 @@ public class Mano extends Mazo {
 
 	public int valorMano() {
 		int valor=0;
+		int numAses=0;
 		for(Carta c : this.cartas) {
+			if(c.getNumero()==1) {
+				numAses++;
+			}
 			valor = valor + c.getValor();
 		}
+		// comprobamos si he pasado de 21
+		
+		while (numAses>0 && valor>21) {
+			valor=valor-10;
+			numAses--;
+		}
 		return valor;
+	}
+	
+	public boolean finDeJuego() {
+		if(valorMano()>=21) {
+				return true;
+			}
+	
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Mano [" + cartas + "] Valor de la mano: " +valorMano();
+	}
+	
+	public void pedirCarta(Mazo baraja) throws NoHayMasCartasException, Masde21Exception, HayBlackjackEXception {
+		//recoge la carta que se encuentra en el principio del Mazo
+		Carta c = baraja.solicitarCarta();
+		this.cartas.add(c);
+		if (valorMano()>21) {
+	    throw new Masde21Exception();
+		}
+		if (valorMano()==21) {
+		    throw new HayBlackjackEXception();
+			}
+			
+			
 	}
 	
 }
